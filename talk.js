c6970a61247ae11bbca43e942783cfae7c5860e2
Grégory <gregory.php@gmail.com>
2015-07-25 16:18:51 +0200

    Finale

--- talk.js
+++ talk.js
@@ -16,14 +16,16 @@ var talk = {
 	},
 	init: function(){
 		if(this.location.pathname[1] == "conversations" && this.location.pathname[2] == "add" && this.location.hash == "#talk"){
-			$.getScript("https://gitcdn.xyz/repo/blueimp/JavaScript-MD5/master/js/md5.js");
+			$.getScript("https://gitcdn.xyz/repo/blueimp/JavaScript-MD5/master/js/md5.js", function(){
+				talk.message();
+			});
 		}
 		if(this.location.pathname[1] == "members"){
 			var username = $('.username').eq(0).text().trim();
 			$('.infoBlock').eq(1).find('.pairsJustified').append(
 				'<dl>' +
 					'<dt>Talk:</dt>' +
-					'<dd><a href="/conversations/add?to='+username+'" class="OverlayTrigger">'+username+'</a></dd>' +
+					'<dd><a href="/conversations/add?to='+username+'#talk" class="OverlayTrigger">'+username+'</a></dd>' +
 				'</dl>');
 		}
 		this.listener();
@@ -34,10 +36,24 @@ var talk = {
 		$(document).on('DOMNodeInserted', function(e) {
 			var card = $(e.target);
 			if (e.target.className == 'contenuMemberCard') {
-				card.find('.userLinks').children().eq(1).after('<a href="/conversations/add?to='+card.find('.username').eq(0).text().trim()+'">Talk</a>');
+				card.find('.userLinks').children().eq(1).after('<a href="/conversations/add?to='+card.find('.username').eq(0).text().trim()+'#talk">Talk</a>');
 			}
 		});
 
+	},
+	message: function(){
+		$('#ctrl_title').val('Rejoins moi sur une shoutbox privée !')
+		$('iframe.redactor_textCtrl').contents().find('body').html("Salut !<br><br>Je t'invite à me rejoindre sur une shoutbox privée pour que l'on puisse parler en toute sécurité via <a href='http://tlk.io/"+this.generate()+"'>ce lien</a><br><br>À bientôt ;)");
+		$('iframe.redactor_textCtrl').contents().find('body').select();
+		$('iframe.redactor_textCtrl').contents().find('body').focus();
+	},
+	generate: function(){
+		if(typeof md5 == "function"){
+			return md5(new Date().getTime()).substr(0, 16);
+		}
+		else {
+			console.error("Talk.js - md5.js manquant");
+		}
 	}
 };
 
